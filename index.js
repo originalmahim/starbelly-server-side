315d5c0d16c1a8f92df6b9b57680a07eb96be61e
originalmahim <tareqazizmahim@gmail.com>
2023-11-27 13:55:04 +0600

    Basic Setup compleated for backend server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000 ;
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+
+
+  const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqvmyxo.mongodb.net/?retryWrites=true&w=majority`;
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  
+  
+ 
+
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
