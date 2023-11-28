247bd15b46199a86bf01db09fc0828beda3e68f0
originalmahim <tareqazizmahim@gmail.com>
2023-11-28 20:08:25 +0600

    Created post api to get stripe payment infos

--- index.jsx
+++ index.jsx
@@ -51,6 +51,20 @@ app.get('/package/:id', async(req,res) => {
     res.send(result);
   });
 
+  app.post('/create-payment-intent', async (req,res) => {
+    const {price} = req.body;
+    const amount = parseInt(price * 100);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+      payment_method_types: ['card']
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret
+    })
+  })
+
   app.get('/users', async(req,res) => {
     const users = await usersFile.find().toArray();
     res.send(users)
